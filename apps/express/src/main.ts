de294ed4ffb22142e5cf178faf295a2da8230bb0
DefectingCat <i@defect.ink>
2022-07-20 14:18:44 +0800

    Add express demo

---
+++ apps/express/src/main.ts
@@ -0,0 +1,44 @@
+import express from 'express';
+import helmet from 'helmet';
+import winston from 'winston';
+import expressWinston from 'express-winston';
+
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(helmet());
+}
+
+app.use(
+  expressWinston.logger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json()
+    ),
+  })
+);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.use(
+  expressWinston.errorLogger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json()
+    ),
+  })
+);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+export default app;
