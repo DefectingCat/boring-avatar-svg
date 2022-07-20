b2710adb65698c67d2313ce94b9913d5b9925d50
DefectingCat <i@defect.ink>
2022-07-20 10:30:22 +0800

    Update demo

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,20 +1,50 @@
-import { Button } from 'ui';
+import { css } from '@emotion/css';
 import avatar from 'boring-avatar-svg';
+import { useCallback, useState } from 'react';
+import Button from '../components/Button';
 
 export default function Web() {
-  const defaultAvatar = avatar({
-    variant: 'bauhaus',
-  });
+  const [pic, setPic] = useState(
+    avatar({
+      variant: 'bauhaus',
+    })
+  );
+
+  const handleClick = useCallback((size: number) => {
+    setPic(
+      avatar({
+        variant: 'bauhaus',
+        size,
+      })
+    );
+  }, []);
 
   return (
     <div>
       <h1>Web</h1>
+
+      <div
+        className={css`
+          display: flex;
+          align-items: center;
+          margin-bottom: 1rem;
+
+          & button {
+            flex: 0 1;
+            margin-right: 1rem;
+          }
+        `}
+      >
+        <Button onClick={() => handleClick(40)}>small</Button>
+        <Button onClick={() => handleClick(80)}>medium</Button>
+        <Button onClick={() => handleClick(120)}>large</Button>
+      </div>
+
       <div
         dangerouslySetInnerHTML={{
-          __html: defaultAvatar,
+          __html: pic,
         }}
       ></div>
-      <Button />
     </div>
   );
 }
